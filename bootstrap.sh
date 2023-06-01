5ae85bb303a3e2429ffe8d67df3fa59895c995b8
Max Mickey <max.mickey1991@gmail.com>
2023-06-01 12:11:31 -0500

    update bootstrap.sh to use new compilation flags

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,13 +9,13 @@ mkdir bootstrap
 ./compile-libc -c src/randy/main.randy -o out/randy -v --ir-comments
 
 # Use version 0 to compile version 1
-./out/randy src/randy/main.randy > bootstrap/randy1.s
-gcc -g3 -c bootstrap/randy1.s -o bootstrap/randy1.o
+./out/randy -c src/randy/main.randy -o bootstrap/randy1.s
+as -c bootstrap/randy1.s -o bootstrap/randy1.o
 ld -o bootstrap/randy1 bootstrap/randy1.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 
 # Use version 1 to compile version 2
-bootstrap/randy1 src/randy/main.randy > bootstrap/randy2.s
-gcc -g3 -c bootstrap/randy2.s -o bootstrap/randy2.o
+bootstrap/randy1 -c src/randy/main.randy -o bootstrap/randy2.s
+as -g3 -c bootstrap/randy2.s -o bootstrap/randy2.o
 ld -o bootstrap/randy2 bootstrap/randy2.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 
 
