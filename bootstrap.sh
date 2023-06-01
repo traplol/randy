0bdf64afbc4b62b677b3e2e06392dde7a8339c5f
Max Mickey <max.mickey1991@gmail.com>
2023-06-01 16:33:08 -0500

    compiler assembles and links itself with 'as' and 'ld' now

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,19 +6,19 @@ rm -rf bootstrap
 mkdir bootstrap
 
 # Compile version 0
-./compile-libc -c src/randy/main.randy -o out/randy -v --ir-comments
+./compile-libc -c src/randy/main.randy -o out/randy -v
 
 # Use version 0 to compile version 1
-./out/randy -c src/randy/main.randy -o bootstrap/randy1.s
-as -c bootstrap/randy1.s -o bootstrap/randy1.o
-ld -o bootstrap/randy1 bootstrap/randy1.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
+./out/randy -c src/randy/main.randy -o bootstrap/randy1 -ld -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
+#as -c bootstrap/randy1.s -o bootstrap/randy1.o
+#ld -o bootstrap/randy1 bootstrap/randy1.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 
 # Use version 1 to compile version 2
-bootstrap/randy1 -c src/randy/main.randy -o bootstrap/randy2.s
-as -g3 -c bootstrap/randy2.s -o bootstrap/randy2.o
-ld -o bootstrap/randy2 bootstrap/randy2.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
+bootstrap/randy1 -c src/randy/main.randy -o bootstrap/randy2 -ld -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
+#as -g3 -c bootstrap/randy2.s -o bootstrap/randy2.o
+#ld -o bootstrap/randy2 bootstrap/randy2.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 
-if [[ -f "randy" ]]; then
+if [[ -f "bin/randy" ]]; then
     cp bin/randy bin/randy.bak
 fi
 
