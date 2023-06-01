ccb22abb722999a8d5a7d8f22b3df12ea5ae6024
Max Mickey <max.mickey1991@gmail.com>
2023-06-01 14:47:58 -0500

    make the generated code runnable

--- src/python/bench_codegen.py
+++ src/python/bench_codegen.py
@@ -3,7 +3,7 @@ import random
 id_n = 0
 
 def permutate(idents):
-    ops = ["*", "+", "-", "/", "and", "or", "%"]
+    ops = ["*", "+", "-", "and", "or"] # no divide or mod because divide by zero...
     n_sub_exprs = random.randint(2, 20);
     tokens = []
     tokens.append(random.choice(idents))
@@ -14,21 +14,29 @@ def permutate(idents):
         tokens.append(lhs)
         tokens.append(op)
     tokens.append(random.choice(idents))
-    return " ".join(tokens)
+    return tokens[0], " ".join(tokens)
 
-def make_proc():
-    global id_n
-    id_n += 1
+def make_proc(id):
     ident = f"proc_{id_n}"
     idents = random.sample(list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
                            random.randint(3, 15))
     print(f"proc {ident} {', '.join(idents)} in")
     for _ in range(random.randint(5, 100)):
-        print(f"    {permutate(idents)};")
-    print(f"    return {random.choice(idents)};")
+        var, line = permutate(idents)
+        print(f"    {line};")
+    print(f"    return {var};")
     print(f"end")
+    return ident, idents
 
+print(f"extern printf fmt, ...;")
+procs = []
 for _ in range(1000):
-    make_proc();
+    procs.append(make_proc(id_n));
+    id_n += 1
 
-print("proc main in end")
+print(f"proc main in")
+print(f"    var xxx = 0;")
+for name, params in procs:
+    print(f"    xxx = xxx + {name}({', '.join(list(map(str, range(len(params)))))});")
+print(f"    printf(\"xxx = %d\\n\", xxx);")
+print(f"end")
