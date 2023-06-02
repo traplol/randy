cee03b4bc8b70d169a571189b6e96df6fb6c3560
Max Mickey <max.mickey1991@gmail.com>
2023-06-02 18:53:17 -0500

    no longer need include hack

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,12 +9,18 @@ mkdir bootstrap
 ./compile-libc -c src/randy/main.randy -o out/randy -v
 
 # Use version 0 to compile version 1
-./out/randy -c src/randy/main.randy -o bootstrap/randy1 -ld -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
+./out/randy -c src/randy/main.randy \
+            -o bootstrap/randy1 \
+            -I include \
+            -ld -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 #as -c bootstrap/randy1.s -o bootstrap/randy1.o
 #ld -o bootstrap/randy1 bootstrap/randy1.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 
 # Use version 1 to compile version 2
-bootstrap/randy1 -c src/randy/main.randy -o bootstrap/randy2 -ld -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
+bootstrap/randy1 -c src/randy/main.randy \
+                 -o bootstrap/randy2 \
+                 -I include \
+                 -ld -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 #as -g3 -c bootstrap/randy2.s -o bootstrap/randy2.o
 #ld -o bootstrap/randy2 bootstrap/randy2.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 
