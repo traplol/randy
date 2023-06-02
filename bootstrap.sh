530f49d13f9fe0d2eb12c942e54b3eeb813b0c70
Max Mickey <max.mickey1991@gmail.com>
2023-06-02 09:06:11 -0500

    should probably keep versions of the bin in vcs

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,10 @@ bootstrap/randy1 -c src/randy/main.randy -o bootstrap/randy2 -ld -dynamic-linker
 #as -g3 -c bootstrap/randy2.s -o bootstrap/randy2.o
 #ld -o bootstrap/randy2 bootstrap/randy2.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 
+if [[ -f "bin/randy.bak" ]]; then
+    cp bin/randy.bak bin/randy.bak2
+fi
+
 if [[ -f "bin/randy" ]]; then
     cp bin/randy bin/randy.bak
 fi
