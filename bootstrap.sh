e96482157c46e90aeeaa32556a01651af00b9d87
Max Mickey <max.mickey1991@gmail.com>
2023-06-03 07:26:09 -0500

    bootstrap script cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,23 +6,27 @@ rm -rf bootstrap
 mkdir bootstrap
 
 # Compile version 0
-./compile-libc -c src/randy/main.randy -o out/randy -v
+bin/randy -c src/randy/main.randy \
+          -o bootstrap/randy0 \
+          -I include \
+          -v \
+          -ld -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 
 # Use version 0 to compile version 1
-./out/randy -c src/randy/main.randy \
-            -o bootstrap/randy1 \
-            -I include \
-            -ld -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
-#as -c bootstrap/randy1.s -o bootstrap/randy1.o
-#ld -o bootstrap/randy1 bootstrap/randy1.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
+bootstrap/randy0 -c src/randy/main.randy \
+                 -o bootstrap/randy1 \
+                 -I include \
+                 -v \
+                 -ld -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
 
 # Use version 1 to compile version 2
 bootstrap/randy1 -c src/randy/main.randy \
                  -o bootstrap/randy2 \
                  -I include \
+                 -v \
                  -ld -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
-#as -g3 -c bootstrap/randy2.s -o bootstrap/randy2.o
-#ld -o bootstrap/randy2 bootstrap/randy2.o -dynamic-linker /home/max/workspace/musl-1.2.4/lib/libc.so -lc
+
+cmp bootstrap/randy1.s bootstrap/randy2.s || exit 1
 
 if [[ -f "bin/randy.bak" ]]; then
     cp bin/randy.bak bin/randy.bak2
@@ -32,4 +36,5 @@ if [[ -f "bin/randy" ]]; then
     cp bin/randy bin/randy.bak
 fi
 
+# If cmp succeeds then we can, in theory, close the bootstrap loop
 cp bootstrap/randy2 bin/randy
